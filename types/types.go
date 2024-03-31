2f2ae8c61aecf49fa9cec324aa39c51354ac62a7
for <nithins674@gmail.com>
2024-03-31 15:45:25 +0100

    Add types package with ImageData, InvertedIndex, AppDataKey, DocumentVector, and JsonData structs

---
+++ types/types.go
@@ -0,0 +1,26 @@
+package types
+
+type ImageData struct {
+	ID                 string   `json:"id"`
+	URL                string   `json:"url"`
+	Title              string   `json:"title"`
+	RelatedImageTags   []string `json:"related_image_tags"`
+	AnnotatedImageTags []string `json:"annotated_image_tags"`
+}
+
+type InvertedIndex struct {
+	TF map[string]int `json:"tf"`
+	DF int            `json:"df"`
+}
+
+type AppDataKey struct{}
+
+type DocumentVector map[string]float64
+
+type JsonData struct {
+	DocumentInfoMap   map[string]ImageData
+	InvertedIndexMap  map[string]InvertedIndex
+	TotalDocs         int
+	DocumentFrequency map[string]int
+	DocumentVectors   map[string]DocumentVector
+}
