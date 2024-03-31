88a1e9aa73c2ba883ed5be20c21c334d23d84834
for <nithins674@gmail.com>
2024-03-31 17:24:18 +0100

    Refactor search results layout and add scroll functionality

--- frontend/components/SearchResults.tsx
+++ frontend/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { SearchData } from "../types";
 
 import "./SearchResults.css";
@@ -20,24 +21,106 @@ function SearchResults({ searchResults }: SearchResultsProps) {
   }
 
   // split the search results into 3 even columns
-  const column1: SearchData[] = [];
-  const column2: SearchData[] = [];
-  const column3: SearchData[] = [];
+  const column1Lg: SearchData[] = [];
+  const column2Lg: SearchData[] = [];
+  const column3Lg: SearchData[] = [];
 
   searchResults.forEach((result, index) => {
     if (index % 3 === 0) {
-      column1.push(result);
+      column1Lg.push(result);
     } else if (index % 3 === 1) {
-      column2.push(result);
+      column2Lg.push(result);
     } else {
-      column3.push(result);
+      column3Lg.push(result);
     }
   });
 
+  // split the search results into 2 even columns
+  const column1Md: SearchData[] = [];
+  const column2Md: SearchData[] = [];
+
+  searchResults.forEach((result, index) => {
+    if (index % 2 === 0) {
+      column1Md.push(result);
+    } else {
+      column2Md.push(result);
+    }
+  });
+
+  useEffect(() => {
+    const scrollHere = document.getElementById("scrollHere");
+    if (scrollHere) {
+      scrollHere.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [searchResults]);
+
+  useEffect(() => {}, []);
+
   return (
-    <div className="gridContainer">
-      <div className="gridItem">
-        {column1.map((result, index) => (
+    <>
+      <div id="scrollHere" />
+      <div className="gridContainerLg">
+        <div className="gridItem">
+          {column1Lg.map((result, index) => (
+            <div key={`${result.title}-${index}`}>
+              <img
+                src={cleanImageUrl(result.url, 400)}
+                alt={result.title}
+                className="image"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="gridItem">
+          {column2Lg.map((result, index) => (
+            <div key={`${result.title}-${index}`}>
+              <img
+                src={cleanImageUrl(result.url, 400)}
+                alt={result.title}
+                className="image"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="gridItem">
+          {column3Lg.map((result, index) => (
+            <div key={`${result.title}-${index}`}>
+              <img
+                src={cleanImageUrl(result.url, 400)}
+                alt={result.title}
+                className="image"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="gridContainerMd">
+        <div className="gridItem">
+          {column1Md.map((result, index) => (
+            <div key={`${result.title}-${index}`}>
+              <img
+                src={cleanImageUrl(result.url, 400)}
+                alt={result.title}
+                className="image"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="gridItem">
+          {column2Md.map((result, index) => (
+            <div key={`${result.title}-${index}`}>
+              <img
+                src={cleanImageUrl(result.url, 400)}
+                alt={result.title}
+                className="image"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="gridContainerSm">
+        {searchResults.map((result, index) => (
           <div key={`${result.title}-${index}`}>
             <img
               src={cleanImageUrl(result.url, 400)}
@@ -47,29 +130,7 @@ function SearchResults({ searchResults }: SearchResultsProps) {
           </div>
         ))}
       </div>
-      <div className="gridItem">
-        {column2.map((result, index) => (
-          <div key={`${result.title}-${index}`}>
-            <img
-              src={cleanImageUrl(result.url, 400)}
-              alt={result.title}
-              className="image"
-            />
-          </div>
-        ))}
-      </div>
-      <div className="gridItem">
-        {column3.map((result, index) => (
-          <div key={`${result.title}-${index}`}>
-            <img
-              src={cleanImageUrl(result.url, 400)}
-              alt={result.title}
-              className="image"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
