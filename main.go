feec4762e94f4b8bd1732695b300bc4a3972ebb6
for <nithins674@gmail.com>
2024-03-31 00:26:56 +0000

    Remove unnecessary print statement in searchHandler function

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"math"
 	"net/http"
@@ -195,8 +194,6 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query().Get("q")
 
 	queryTerms := tokenizeText(query)
-
-	fmt.Println(queryTerms)
 
 	idf := make(map[string]float64)
 	for _, term := range queryTerms {
