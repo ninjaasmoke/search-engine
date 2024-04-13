a9a143a7f91fddc0011625601b725a39dcb6a5fb
for <nithins674@gmail.com>
2024-04-13 13:39:55 +0100

    Add link to about page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import SearchInputComponent from "../components/SearchInput.component";
 
 import { useEffect, useState } from "react";
 import "./App.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function App() {
   const navigate = useNavigate();
@@ -48,63 +48,17 @@ function App() {
           />
         </div>
       </div>
-      <div
+      <Link
+        to={`/about`}
         style={{
-          padding: 20,
-          // textAlign: "center",
-          maxWidth: 680,
+          padding: "10px",
+          marginBottom: "20px",
+          fontSize: "0.8rem",
+          textDecoration: "underline",
         }}
       >
-        <p>
-          A project developed by{" "}
-          <a target="_blank" href="https://github.com/ninjaasmoke">
-            Nithin Sai Kirumani Jagadish
-          </a>{" "}
-          as part of{" "}
-          <a
-            target="_blank"
-            href="https://www.dcu.ie/engineeringandcomputing/mechanics-search"
-          >
-            CA6005 Mechanics of Search
-          </a>{" "}
-          project submitted to{" "}
-          <a target="_blank" href="https://dcu.ie">
-            Dublin City University
-          </a>
-          . The goal of this project is to develop an image based search engine
-          using annotations and image features.
-        </p>
-        <p>
-          This project uses images from{" "}
-          <a target="_blank" href="https://unsplash.com">
-            Unsplash
-          </a>{" "}
-          and the license to use the images can be found{" "}
-          <a target="_blank" href="https://unsplash.com/license">
-            here
-          </a>
-          .
-        </p>
-        <p>
-          This search engine is <strong>NOT</strong> a competitor to Unsplash in
-          any way and is developed for educational purposes only.
-        </p>
-        <p>
-          <strong>ALL</strong> images you see in this website belong to Unsplash
-          alone.
-        </p>
-        <p>
-          In the off chance that anyone from Unsplash is viewing this, please{" "}
-          <a
-            target="_blank"
-            href="mailto:nithin.kirumanijagadish2@mail.dcu.ie"
-            style={{ textDecoration: "underline" }}
-          >
-            email me
-          </a>{" "}
-          if you have any concerns.
-        </p>
-      </div>
+        read about this
+      </Link>
     </div>
   );
 }
