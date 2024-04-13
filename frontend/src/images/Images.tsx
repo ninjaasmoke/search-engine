48b91ad4b28a73c74c584f100cc3de1fd1657f8b
for <nithins674@gmail.com>
2024-04-13 14:22:25 +0100

    Add Images module with URLs for various images

---
+++ frontend/src/images/Images.tsx
@@ -0,0 +1,12 @@
+const Images = {
+  cat_with_sunglasses: new URL("./cat_with_sunglasses.png", import.meta.url)
+    .href,
+  flower_arrangement: new URL("./flower_arrangement.png", import.meta.url).href,
+  flowers_near_eiffel_tower: new URL(
+    "./flowers_near_eiffel_tower.png",
+    import.meta.url
+  ).href,
+  sunset: new URL("./sunset.png", import.meta.url).href,
+};
+
+export default Images;
