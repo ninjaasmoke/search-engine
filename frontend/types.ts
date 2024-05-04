b3d083c21b3efbcd7ac39c1196e218b777e246dc
for <nithins674@gmail.com>
2024-05-04 18:22:25 +0100

    Update SearchData interface and add SearchResponse interface in frontend/types.ts

--- frontend/types.ts
+++ frontend/types.ts
@@ -1,7 +1,14 @@
+
 export interface SearchData {
   id: string;
   url: string;
   title: string;
   related_image_tags: string[];
   annotated_image_tags: string[];
+}
+
+export interface SearchResponse {
+  Query: string;
+  CorrectedQuery: string;
+  Documents: SearchData[];
 }
